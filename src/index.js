7a720f94135edd3c8274ddd9334cc30329b39a52
juanmartin1413 <55548708+juanmartin1413@users.noreply.github.com>
2022-01-26 18:44:37 -0300

    Se crea carpeta especifica para NavBar y demas sugerencias del profe

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import NavBar from './components/NavBar';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js'
@@ -11,7 +10,7 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 
 ReactDOM.render(
   <React.StrictMode>
-    <NavBar />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
