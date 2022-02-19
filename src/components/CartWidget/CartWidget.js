3aa4f68db05147970773c4cbf7d0d602a9315c8f
juanmartin1413 <55548708+juanmartin1413@users.noreply.github.com>
2022-02-18 23:09:34 -0300

    se borra directorio para subir nuevos archivos

--- src/components/CartWidget/CartWidget.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './CartWidget.css';
-import { FaShoppingCart } from 'react-icons/fa';
-
-export default function CartWidget(){
-    return (
-        <FaShoppingCart/>
-    );
-};
